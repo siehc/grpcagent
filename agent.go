69e0df41602b1c14c491d032b0454d8b8ec939e8
Nick <nick@gamma-gaming.com>
2019-03-25 16:32:01 +0800

    add metadata support

--- agent.go
+++ agent.go
@@ -1,21 +1,24 @@
 package grpcagent
 
-import "github.com/golang/protobuf/proto"
+import (
+	"github.com/golang/protobuf/proto"
+	"google.golang.org/grpc/metadata"
+)
 
 type Agent struct {
 	client  interface{}
-	handler func(interface{}, string, interface{}) (proto.Message, error)
+	handler func(interface{}, string, interface{}, metadata.MD) (proto.Message, error)
 }
 
-func (c *Agent) Request(method string, data interface{}) (proto.Message, error) {
-	return c.handler(c.client, method, data)
+func (c *Agent) Request(method string, data interface{}, header metadata.MD) (proto.Message, error) {
+	return c.handler(c.client, method, data, header)
 }
 
 func (c *Agent) Client() interface{} {
 	return c.client
 }
 
-func CreateAgent(client interface{}, handler func(interface{}, string, interface{}) (proto.Message, error)) *Agent {
+func CreateAgent(client interface{}, handler func(interface{}, string, interface{}, metadata.MD) (proto.Message, error)) *Agent {
 	return &Agent{
 		client,
 		handler,
